8a5f2531d2fb53cb736329137616dd4b25d1f944
Jan Elholm-Christensen <jan.elholm@fellowmind.dk>
2024-09-21 09:32:09 +0200

    Added comment plugin

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set('n', '<leader>n', '<cmd>bn<cr>')
+vim.keymap.set('n', '<leader>p', '<cmd>bp<cr>')
+vim.keymap.set('n', '<leader>x', '<cmd>bd<cr>')
+
+-- yank to clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
