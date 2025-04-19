7cbc98c89bddd4cc922fb4fabea66a2f565a9f24
Jan Elholm-Christensen <jan.elholm@outlook.dk>
2025-04-19 21:05:12 +0200

    Installed conform plugin for use of prettier

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set('n', '<leader>n', '<cmd>bn<cr>')
-vim.keymap.set('n', '<leader>p', '<cmd>bp<cr>')
-vim.keymap.set('n', '<leader>x', '<cmd>bd<cr>')
+vim.keymap.set("n", "<leader>n", "<cmd>bn<cr>")
+vim.keymap.set("n", "<leader>p", "<cmd>bp<cr>")
+vim.keymap.set("n", "<leader>x", "<cmd>bd<cr>")
 
 -- yank to clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>cf", function()
+  require("conform").format({
+    lsp_format = "fallback",
+  })
+end, { desc = "Format with conform" })
